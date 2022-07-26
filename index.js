c93216a1e9177768d82e2b545472635dbe5570b6
Lesslie Houck <Lesslie00@outlook.com>
2022-07-25 18:21:05 -0700

    commit tree fd608dfeee8cbded61766a2be939d306392b23e3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return ['Antonia', 'Nuru']
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return ['Amari', 'Mo']
+}
